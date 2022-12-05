d4a77e53605f0956c37db79693a28b3d562dfdf6
Matthew Antirix <matthew.antirix@gmail.com>
2022-12-05 10:21:41 +0100

    responsive for grid / table display

--- src/App.jsx
+++ src/App.jsx
@@ -152,9 +152,9 @@ const ItemBoxContent = () => (
   <div>
     {test_data.map(item => (
       <Unit.ItemBox key={item.toString()}>
-        <div class="box_title"> <Icon path={mdiFinance} size={1.2}/> {item.name.slice(0, 25)} </div>
-        <div class="box_type"> Type: {item.type.slice(0, 25)} </div> 
-        <div class="box_topics"> Topic: {item.topics.join(", ").slice(0, 25)} </div>
+        <div class="box_title"> <span class="box_icon"><Icon path={mdiFinance} size={1.0}/></span> {item.name.slice(0, 20)} </div>
+        <div class="box_type"> Type: {item.type.slice(0, 20)} </div> 
+        <div class="box_topics"> Topic: {item.topics.join(", ").slice(0, 20)} </div>
         <img class="box_img" src={item.img}></img>
         <p class="box_description"> {item.description.slice(0, 100)}... </p>
         <div class="box_authors"> {item.authorList.join(", ").slice(0, 25)} </div>
