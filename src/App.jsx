2c271d7f72490d36e7bd8478200713a0dcfa3b2f
Matthew Antirix <matthew.antirix@gmail.com>
2022-12-29 15:28:43 +0100

    display test fix

--- src/App.jsx
+++ src/App.jsx
@@ -189,8 +189,7 @@ const AppContent = () => {
             {isGrid ? (
                 <ItemBoxContent/>
                 ) : (
-                // <ItemListContent/>
-                <DetailItemContent />
+                <ItemListContent/>
                 )}
         </>
     )
