0d051b837e30f9004cfb81a1a52d525d3fe7d1b1
Ondřej Brůha <bruhaon@gmail.com>
2023-01-08 20:33:32 +0100

    label

--- src/bricks/CreateDiagram.jsx
+++ src/bricks/CreateDiagram.jsx
@@ -52,11 +52,17 @@ function CreateDiagram(props) {
 
     return (
         <div className="createDiagram">
+            <label>Name</label>
             <input onChange={() => { handleChange() }} id="name" value={form.name}></input>
+            <label>Type</label>
             <input onChange={() => { handleChange() }} id="type" value={form.type}></input>
+            <label>Author</label>
             <input onChange={() => { handleChange() }} id="author" value={form.authorList[0]}></input>
+            <label>Description</label>
             <input onChange={() => { handleChange() }} id="description" value={form.description}></input>
+            <label>Topics</label>
             <input onChange={() => { handleChange() }} id="topics" value={form.topics[0]}></input>
+            <label>Image URL</label>
             <input onChange={() => { handleChange() }} id="img" value={form.img}></input>
             <button onClick={() => { submit(); props.handleClose(false); }}>Submit</button>
             <button onClick={() => { props.handleClose(false); }}>Cancel</button>
