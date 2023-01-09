f1fd96789e011f51b85b0b8dd6e7fb9e6f5a8503
Ondřej Brůha <bruhaon@gmail.com>
2023-01-09 21:39:38 +0100

    fix classsname

--- src/bricks/detail.jsx
+++ src/bricks/detail.jsx
@@ -9,24 +9,24 @@ function Detail(props) {
         <div key="detail" className="detail">
             <div className="detailHeader">
                 <button onClick={()=>{props.next();}}>Next diagram</button>
-                <div class="box_title">
-                    <span class="box_icon">
+                <div className="box_title">
+                    <span className="box_icon">
                         <Icon path={mdiFinance} size={1.0} />
                     </span>{" "}
                     {item.name.slice(0, 20)}{" "}
                 </div>
             </div>
             <div className="detailUnderHeader">
-                <div class="box_type"> Type: {item.type.slice(0, 20)} </div>
-                <div class="box_topics">
+                <div className="box_type"> Type: {item.type.slice(0, 20)} </div>
+                <div className="box_topics">
                     {" "}
                     Topic: {item.topics.join(", ").slice(0, 20)}{" "}
                 </div>
-                <div class="box_authors"> Author: {item.authorList.join(", ").slice(0, 25)} </div>
+                <div className="box_authors"> Author: {item.authorList.join(", ").slice(0, 25)} </div>
             </div>
             <div className="detailBody">
-                <img class="box_img" src={item.img} alt={item.name.slice(0, 20)}></img>
-                <p class="box_description"> {item.description.slice(0, 100)}... </p>
+                <img className="box_img" src={item.img} alt={item.name.slice(0, 20)}></img>
+                <p className="box_description"> {item.description.slice(0, 100)}... </p>
             </div>
             <button onClick={()=>{props.handleClose(false)}}>
                 Close
