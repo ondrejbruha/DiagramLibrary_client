569a706d44a2c8a49ebbdae26d2d18fd132f6e9c
Ondřej Brůha <bruhaon@gmail.com>
2023-01-09 19:27:47 +0100

    funkčnost detail

--- src/bricks/DiagramGridList.jsx
+++ src/bricks/DiagramGridList.jsx
@@ -4,11 +4,13 @@ import * as Unit from '../Reusable'
 import Icon from '@mdi/react';
 import {mdiFinance} from '@mdi/js';
 import UpdateDiagram from './update';
+import Detail from './detail';
 
 
 function ItemBoxContent(props) {
     const [update, setUpdate] = useState(false);
     const [itm, setItm] = useState(props[0]);
+    const [detail, setDetail] = useState(false);
     return(
         <div id="box_div">
             {props.diagramList.map(item => (
@@ -18,11 +20,13 @@ function ItemBoxContent(props) {
                     <div class="box_topics"> Topic: {item.topics.join(", ").slice(0, 20)} </div>
                     <img class="box_img" src={item.img} alt={item.name.slice(0, 20)}></img>
                     <p class="box_description"> {item.description.slice(0, 100)}... </p>
-                    <button onClick={()=>{setUpdate(true); setItm(item)}}>Update</button>
+                    <button onClick={()=>{setItm(item);setUpdate(true); }}>Update</button>
+                    <button onClick={()=>{setItm(item);setDetail(true);}}>Detail</button>
                     <div class="box_authors"> {item.authorList.join(", ").slice(0, 25)} </div>
                 </Unit.ItemBox>
             ))}
-            {update ? <UpdateDiagram data={itm} handleClose={setUpdate}></UpdateDiagram> : <></>}
+            {update ? <UpdateDiagram key="update" data={itm} handleClose={setUpdate}></UpdateDiagram> : null}
+            {detail ? <Detail key="detail" data={itm} handleClose={setDetail}></Detail>: null}
         </div>
     )
 }
---
+++ src/bricks/detail.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Icon from "@mdi/react";
+import { mdiFinance } from "@mdi/js";
+import "../theme/detail.css"
+
+function Detail(props) {
+    let item = props.data;
+    return (
+        <div key="detail" className="detail">
+            <div className="detailHeader">
+                <div class="box_title">
+                    <span class="box_icon">
+                        <Icon path={mdiFinance} size={1.0} />
+                    </span>{" "}
+                    {item.name.slice(0, 20)}{" "}
+                </div>
+            </div>
+            <div className="detailUnderHeader">
+                <div class="box_type"> Type: {item.type.slice(0, 20)} </div>
+                <div class="box_topics">
+                    {" "}
+                    Topic: {item.topics.join(", ").slice(0, 20)}{" "}
+                </div>
+                <div class="box_authors"> Author: {item.authorList.join(", ").slice(0, 25)} </div>
+            </div>
+            <div className="detailBody">
+                <img class="box_img" src={item.img} alt={item.name.slice(0, 20)}></img>
+                <p class="box_description"> {item.description.slice(0, 100)}... </p>
+            </div>
+            <button onClick={()=>{props.handleClose(false)}}>
+                Close
+            </button>
+        </div>
+    );
+}
+export default Detail;
