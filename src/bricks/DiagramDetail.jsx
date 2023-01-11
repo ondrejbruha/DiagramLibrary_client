36ce6cc31ad64fa11ac9354963986e3b16b6b3eb
Ondrej Bruha <ondrej.bruha@outlook.com>
2023-01-11 12:55:50 +0100

    diagram detail nasazení na původní componentu

--- src/bricks/DiagramDetail.jsx
+++ src/bricks/DiagramDetail.jsx
@@ -2,22 +2,28 @@ import React from 'react';
 import '../theme/main.css';
 
 
-export const DetailItemContent = (props) => (
-    <div class="detail_wrapper">
-        <span class="detail_title">{props.diagramList[0].name}</span>
-        <span class="detail_id">ID: {props.diagramList[0].id}</span>
-        <span class="detail_topics">Topics: {props.diagramList[0].topics.join(", ")}</span>
-        <span class="detail_type">Type: {props.diagramList[0].type}</span>
-
-        <img class="detail_img" src={props.diagramList[0].img} alt={props.diagramList.name}></img>
-
-        <div class="description_wrapper">
-            <span class="detail_description">{props.diagramList[0].description}</span>
-
-            <div class="description_footer">
-                <span class="detail_published_date">Published {props.diagramList[0].publishDate}</span>
-                <span class="detail_authors"> by {props.diagramList[0].authorList.join(", ")}</span>
+export const DetailItemContent = (props) => {
+    return(
+        <div className="detail_wrapper">
+            <button onClick={()=>{props.handleClose(false)}}>
+                Close
+            </button>
+            <button onClick={()=>{props.next();}}>Next diagram</button>
+            <span className="detail_title">{props.data.name}</span>
+            <span className="detail_id">ID: {props.data.id}</span>
+            <span className="detail_topics">Topics: {props.data.topics.join(", ")}</span>
+            <span className="detail_type">Type: {props.data.type}</span>
+    
+            <img className="detail_img" src={props.data.img} alt={props.data.name}></img>
+    
+            <div className="description_wrapper">
+                <span className="detail_description">{props.data.description}</span>
+    
+                <div className="description_footer">
+                    <span className="detail_published_date">Published {(new Date(props.data.publishDate)).toLocaleDateString()}</span>
+                    <span className="detail_authors"> by {props.data.authorList.join(", ")}</span>
+                </div>
             </div>
         </div>
-    </div>
-)
+    );
+}
--- src/bricks/DiagramGridList.jsx
+++ src/bricks/DiagramGridList.jsx
@@ -1,38 +1,39 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import '../theme/main.css';
 import * as Unit from '../Reusable'
 import Icon from '@mdi/react';
-import {mdiFinance} from '@mdi/js';
+import { mdiFinance } from '@mdi/js';
 import UpdateDiagram from './update';
 import Detail from './detail';
+import { DetailItemContent } from './DiagramDetail';
 
 
 function ItemBoxContent(props) {
     const [update, setUpdate] = useState(false);
     const [itm, setItm] = useState(props[0]);
     const [detail, setDetail] = useState(false);
-    const next = ()=>{
+    const next = () => {
         let len = props.diagramList.length;
         let i = props.diagramList.findIndex(e => e === itm);
-        let newItm = props.diagramList[(i+1)%len];
+        let newItm = props.diagramList[(i + 1) % len];
         setItm(newItm);
     };
-    return(
+    return (
         <div id="box_div">
             {props.diagramList.map(item => (
                 <Unit.ItemBox key={item.toString()}>
-                    <div class="box_title"><span class="box_icon"><Icon path={mdiFinance} size={1.0}/></span> {item.name.slice(0, 20)} </div>
+                    <div class="box_title"><span class="box_icon"><Icon path={mdiFinance} size={1.0} /></span> {item.name.slice(0, 20)} </div>
                     <div class="box_type"> Type: {item.type.slice(0, 20)} </div>
                     <div class="box_topics"> Topic: {item.topics.join(", ").slice(0, 20)} </div>
                     <img class="box_img" src={item.img} alt={item.name.slice(0, 20)}></img>
                     <p class="box_description"> {item.description.slice(0, 100)}... </p>
-                    <button onClick={()=>{setItm(item);setUpdate(true); }}>Update</button>
-                    <button onClick={()=>{setItm(item);setDetail(true);}}>Detail</button>
+                    <button onClick={() => { setItm(item); setUpdate(true); }}>Update</button>
+                    <button onClick={() => { setItm(item); setDetail(true); }}>Detail</button>
                     <div class="box_authors"> {item.authorList.join(", ").slice(0, 25)} </div>
                 </Unit.ItemBox>
             ))}
             {update ? <UpdateDiagram key="update" data={itm} handleClose={setUpdate}></UpdateDiagram> : null}
-            {detail ? <Detail next={next} key="detail" data={itm} handleClose={setDetail}></Detail>: null}
+            {detail ? <DetailItemContent next={next} key="detail" data={itm} handleClose={setDetail}/> : null}
         </div>
     )
 }
